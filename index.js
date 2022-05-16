c7503d6711342fd10642185ffcd01ca0ed1c3b0d
emmanuel Lerma <emmanuellerma96@gmail.com>
2022-05-16 09:41:33 -0500

    Cleanup the interface for a more cleaner look

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const userChoiceDisplay = document.createElement('h3');
 const computerChoiceDisplay = document.createElement('h3');
-const resultDisplay = document.createElement('h3');
+const resultDisplay = document.createElement('h4');
+//const matchDisplay = document.createElement('h3');
 const gameGrid = document.getElementById('game');
 gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay);
 
@@ -8,10 +9,13 @@ const choices = ['rock', 'paper', 'scissors'];
 let userChoice;
 let computerChoice;
 
-let playerScore = parseInt(0);
-let computerScore = parseInt(0);
-let roundWinner = '';
+let playerScore = 0;
+let computerScore = 0;
+//let roundWinner = '';
 
+let score = ""
+let result = ""
+const button = document.querySelectorAll('input');
 
 const handleClick = (e) => {
     userChoice = e.target.id
@@ -26,157 +30,85 @@ const generateComputerChoice = () => {
     computerChoiceDisplay.innerHTML = 'Computer choice: ' + computerChoice
 }
 
-function disableButtons() {
-    disableButtons.forEach(elem => {
-        elem.disabled = true
+function disableButton() {
+    button.forEach (elem => {
+        elem.disabled = true;
     })
-}
+    }
 
-for (let i = 0; i < choices.length; i++) {
+
+
+/*for (let i = 0; i < choices.length; i++) {
     const button = document.createElement('button')
     button.id = choices[i]
     button.innerHTML = choices[i]
     button.addEventListener('click', handleClick)
-    gameGrid.appendChild(button)
+    gameGrid.appendChild(button) 
+}
+*/
+
+//const button = document.querySelector('button');
+
+for (let i = 0; i < choices.length; i++) {
+    const button = document.querySelector('input')
+    button.id = choices[i]
+    button.innerHTML = choices[i]
+    button.addEventListener('click', handleClick)
+    gameGrid.appendChild(button) 
 }
 
-
-
 function getResult()  {
+    let result = ""
+    let score = ""
+
+
     if (userChoice === computerChoice) {
         resultDisplay.innerHTML = "It's a Draw!"
+        score = ("<br>Play Score: " + playerScore + "<br>ComputerScore: " + computerScore)
+
     } if (
         (userChoice === 'rock' && computerChoice == 'scissors') ||
         (userChoice === 'scissors' && computerChoice == 'paper') ||
         (userChoice === 'paper' && computerChoice == 'rock') 
      ) {
         resultDisplay.innerHTML = "User wins this round"
-        playerScore += 1;
+        playerScore++;     
+        score = ("<br>Play Score: " + playerScore + "<br>ComputerScore: " + computerScore)
+
+        if (playerScore == 5) {
+            result += "You won the match!"
+            disableButton();
+        }
     } if (
         (computerChoice === 'rock' && userChoice == 'scissors') ||
         (computerChoice === 'scissors' && userChoice == 'paper') ||
         (computerChoice === 'paper' && userChoice == 'rock') 
     ) {
+
         resultDisplay.innerHTML = "Computer takes this round"
-        computerScore += 1;
+        computerScore++;
+        score = ("<br>Play Score: " + playerScore + "<br>ComputerScore: " + computerScore)
+
+        if (computerScore == 5) {
+            result += "You lost the match!"
+            disableButton();
     }
-    checkWinner();
-}
-
-
-
-function checkWinner() {
-    if (computerScore === 5 || playerScore === 5) {
-        if (computerScore === playerScore) {
-            return  "Match tied"
-        } else if (computerScore > playerScore) {
-            return "You lost!"
-        } else {
-            return "You won!"
-        }
-    }
-}
-
-
-
-
-
-
-
-
-/*
-
-const winnerResults ={
-    computer: ["You lost to the computer!", 'red'],
-    player: ["You win the match!", 'green'],
-    tie: ["The match is tied!", 'blue']
-}
-
-
-const getResult = () => {
-    switch (userChoice + computerChoice) {
-        case 'scissorpaper':
-        case 'rockscissors':
-        case 'paperrock':
-            resultDisplay.innerHTML = "You Win!"
-            playerScore++
-            break
-        case 'paperscissors':
-        case 'scissorsrock':
-        case 'rockpaper':
-            resultDisplay.innerHTML = "You Lose!"
-            computerScore++
-            break
-        case 'paperpaper':
-        case 'scissorsscissors':
-        case 'rockrock':
-            resultDisplay.innerHTML = "Tie!"
-            break
-    } 
-} 
-
-
-
-
-
-
-function updateWinner(winner) {
-    resultDisplay.textContent = winnerResults[winner][0];
     
-    userChoice.forEach(button => {
-        button.removeEventListener('click', userChoice)
-    });
+  }
+
+
+//document.getElementById('score').innerHTML = score
+
+document.getElementById('playerScore').innerHTML = ("Player: " + playerScore)
+document.getElementById('compScore').innerHTML = ("Computer: " + computerScore)
+document.getElementById('result').innerHTML = result
+return;
+
+
 }
 
 
-
-function checkWinner() {
-    if (computerScore === 5 || playerScore === 5) {
-        if (computerScore === playerScore) {
-            updateWinner('tie')
-        } else {
-            let win = `${(computerScore > playerScore) ? 'computer' : 'player'}`;
-            updateWinner(win);
-        }
-    }
+function refreshPage(){
+    window.location.reload();
 }
 
-
-
-
-
-function playRound(getResult) {
-    if (userChoice === computerChoice) {
-        roundWinner = 'tie'
-    } if (
-        (userChoice === 'rock' && computerChoice == 'scissors') ||
-        (userChoice === 'scissors' && computerChoice == 'paper') ||
-        (userChoice === 'paper' && computerChoice == 'rock') ||
-    ) {
-        playerScore++
-        roundWinner = 'user'
-    } if (
-        (computerChoice === 'rock' && userChoice == 'scissors') ||
-        (computerChoice === 'scissors' && userChoice == 'paper') ||
-        (computerChoice === 'paper' && userChoice == 'rock') ||
-    ) {
-        computerScore++
-        roundWinner = 'computer'
-    }
-    checkWinner();
-}
-
-
-
-
-
-
-
-
-
-
-function isGameOver() {
-    return playerScore === 5 || computerScore === 5
-}
-
- */
